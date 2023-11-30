5647fd14ee052c17f78ee9ed84505df6a4c87880
unknown <codeswitharif@gmail.com>
2023-11-30 21:28:44 +0700

    task7

--- date.php
+++ date.php
@@ -1,3 +1,18 @@
 <?php 
-echo date("l",strtotime("25 October 2030"));
+function salam($waktu,$nama)
+{
+    return "Selamat $waktu, $nama!";
+}
+
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1><?= salam("Pagi", "Eka")?></h1>
+</body>
+</html>
