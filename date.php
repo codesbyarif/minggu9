c60b188df075ae02697d7b979d57b398d44510f9
unknown <codeswitharif@gmail.com>
2023-11-30 22:11:00 +0700

    task6

--- date.php
+++ date.php
@@ -1,11 +1,3 @@
 <?php 
-function perkalian($angka1, $angka2)
-{
-   return $angka1 * $angka2;
-}
-
-$hasil = perkalian(20, 3);
-echo "Perkalian 20 x 3 adalah $hasil";
-echo "<br>";
-echo "Perkalian 9 x 2 adalah " . perkalian(9,2);
+echo date("l",strtotime("25 October 2030"));
 ?>
